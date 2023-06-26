aec47ec65d8777ac8d9ea50d3d83c10517d4d081
sudaraWishvajith <it21221378@gmail.com>
2023-06-26 12:11:00 +0530

    Add handleSubmit funciton
    when user submit a task, create new toDo object and add to toDos array

--- todo_list/src/App.js
+++ todo_list/src/App.js
@@ -3,27 +3,47 @@ import "./App.css";
 const App = () => {
   const [todos, setTodos] = React.useState([]);
   const [todo, setTodo] = React.useState("");
-  
+
   // Add the handlesubmit code here
-  
-  
+  function handlesubmit(e) {
+    e.preventDefault();
+
+    const newTodo = {
+      id: new Date().getTime(),
+      text: todo.trim(),
+      completed: false,
+    };
+
+    if (newTodo.text.length > 0) {
+      setTodos([...todos].concat(newTodo));
+      setTodo("");
+    } else {
+      alert("Enter valid Task");
+      setTodo("");
+    }
+
+  }
+
   // Add the deleteToDo code here
 
-  
+
   // Add the toggleComplete code here
 
-  
+
   // Add the submitEdits code here
 
-  
-return(
-<div className ="App">
-<h1>Todo List</h1>
-<form>
-<input type ="text" align ="right" />
-<button type ="submit">Add Todo</button>
-</form>
-</div>
-);
+
+  return (
+    <div className="App">
+      <h1>Todo List</h1>
+      <form onSubmit={handlesubmit}>
+        <input type="text" align="right"
+          onChange={(e) => setTodo(e.target.value)}
+          placeholder="Enter new Task"
+          value={todo} />
+        <button type="submit">Add Todo</button>
+      </form>
+    </div>
+  );
 };
 export default App;
