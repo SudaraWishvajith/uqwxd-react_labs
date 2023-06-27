b33194ae89bab53a3dbe2173a827d2a2aa28b123
SudaraWishvajith <it21221378@my.sliit.lk>
2023-06-27 10:13:04 +0530

    Create MainPanel component
    this includes DivPanel and MyButton components

---
+++ react-redux-master/src/components/MainPanel.js
@@ -0,0 +1,14 @@
+import react from 'react'
+import MyButton from './MyButton'
+import DivPanel from './DivPanel'
+
+const MainPanel = ()=>{
+    return(
+        <div>
+            This is main panel <MyButton></MyButton>
+            <DivPanel></DivPanel>
+        </div>
+    )
+}
+
+export default MainPanel;
